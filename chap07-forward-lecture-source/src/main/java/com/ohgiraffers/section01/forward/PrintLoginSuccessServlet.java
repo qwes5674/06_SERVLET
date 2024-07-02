124fbeb038d9b65e837a782ed656e50ab281eb6a
qwes5674 <qwes5674@gmail.com>
2024-07-02 15:26:40 +0900

    forward

--- chap07-forward-lecture-source/src/main/java/com/ohgiraffers/section01/forward/PrintLoginSuccessServlet.java
+++ chap07-forward-lecture-source/src/main/java/com/ohgiraffers/section01/forward/PrintLoginSuccessServlet.java
@@ -46,5 +46,17 @@ public class PrintLoginSuccessServlet extends HttpServlet {
 
         out.flush();
         out.close();
+
+        /*
+        * 기본적으로 변수의 기본 스코프 메소드 (= 해당 페이지) 이기 떄문에 다른 페이지 (= 서블릿)
+        * 으로 데이터를 공유 할 수 없다.
+        * 하지만 forward방식은 request와 response를 포함하여 위임하므로 request 정보를
+        * 저장하여, forward하면 위임받은 서블릿에서도 위임한 서블릿의 정보를 공유할 수 있다.
+        * 이때 forward 받은 서블릿의 존재를 클라이언트가 알 필요가 없기 떄문에
+        * URL이 변경되지 않는다. (결과하면만 받으면 되기 떄문에)
+        * => forward 방식의 특징이 요청시 서버로 전송한 데이터가 남아있는 상태로 새로고침(= 재요청)
+        * 을 하게 되면 동일한 요청을 반복하여 데이터 베이스에 insert 하는 등의 행위가 중복될 가능성이 잇다.
+        * -> 다른페이지 전환 방식인 sendRedirect를 이용한다.
+        * */
     }
 }
